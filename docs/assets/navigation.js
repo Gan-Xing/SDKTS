b6e14bf46f070755f132088dce3b3c8abcda13d9
Gan-Xing <599153574@qq.com>
2023-11-03 15:45:09 +0800

    build: docs change

--- docs/assets/navigation.js
+++ docs/assets/navigation.js
@@ -1,1 +1,1 @@
-window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAE4uuVipJrShRslLyrXTOSSwuVtJRKkgsyQAKJIO4qcX6UAm9jJLcHKBsdmZeipKVoZFFbSwAMUbVPD0AAAA="
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAE4uuVipJrShRslJKSU1LLM0pUdJRKkgsyQAKJOckFhenFutDJfQySnJzgLLZmXkpSlaGRha1sQAM4TeJPQAAAA=="
