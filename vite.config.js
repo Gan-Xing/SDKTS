f9b8932c3d76b9d77d6155c9f2eab959dfd528d5
Gan-Xing <599153574@qq.com>
2023-11-03 15:48:13 +0800

    feat(tsconfig.json): add cesiumjs config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
   build: {
     lib: {
       entry: "src/index.ts",
-      name: "sdkts",
+      name: "CesiumSDK",
       fileName: "index",
+    },
+    rollupOptions: {
+      external: ["cesium"],
+      output: {
+        globals: {
+          cesium: "Cesium",
+        },
+      },
     },
   },
 });
